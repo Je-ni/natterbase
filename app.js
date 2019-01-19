2c0c686241e8d17322300b709052d21e3def9a0b
je-ni <jenniferolibie@gmail.com>
2019-01-19 02:38:08 +0100

    setup app.js

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const userRoute = require('./routes/userRoute');
+const countryRoute = require('./routes/countryRoute');
+// Set up the express app
+const app = express();
+
+// Log requests to console
+app.use(logger('dev'));
+
+// Parse incoming requests data (https://github.com/expressjs/body-parser)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//Routes which should handle requests
+app.use('/user', userRoute);
+app.use('/country', countryRoute);
+
+// Setup a route that sends back an apology message in JSON format.
+app.get('/', (req, res) => res.send({
+  status: 200,
+  message: 'I apologise once again for this lateness in submission.',
+}));
+
+const port = parseInt(process.env.PORT, 10) || 8000;
+app.listen(port, () => console.log(`App started on port ${port}`));
+
+module.exports = app;
